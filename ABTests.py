c1e1d1b56990abd99fe6eeb2adf7be0619e8b6e7
nishanthmhegde <=>
2019-06-08 20:35:11 +0530

    Added AB Tests

---
+++ ABTests.py
@@ -0,0 +1,29 @@
+AB Tests are useful in determining changes to a website. A is the previous state of thr website and B is the new state of the website 
+which we want to test out. AB Tests have to be carried out for a decent amount of time so that all types of customers can be accounted
+for. AB Tests are generally used for accounting for changes for E-Commerce websites.
+They can be used for determining the following types of changes:
+
+We can use t-tests/ t-statistics and p-values to determine which of A or B might be better. 
+1. Pricing changes
+2. UI changes
+3. UI workflow changes
+4. Order quantity
+5. Order amount
+
+We should, however be careful while selecting the time frame for executing the tests and the customer group we target.
+Few pointers that have to be kept in mind:
+1. Time or month during which we start our test. We should be wary of the holiday seasons like Diwali or Christmas 
+because the spending trends changes during these festival seasons.
+2. We should carefully choose the customer group we use for tests. If we have chosen a group of peple for testing 
+the website ourseves, we should have proper distribution of customers for both A and B. We cannot afford to have
+all good customers in one group and bad customers in another. 
+3. We should be aware of the random variance which is inevitable to come up in our tests. There is no way we can totally
+prevent it. We should inform our business owners about the random variance. 
+4. We should learn to take these results with a pinch of salt because of the random variance and probable presence
+of bots who can skew our results or the time during which we conduct the test.
+5. There is a high chance that old customers might get attracted to the new UI and have a few more clicks or purchases
+	but the new customers might not recognise the changes and be neutral. But after a few days, the old customers might
+	like the old UI better and the new customers might not like the website. It can go both ways.
+6. Some times less favourable or more favorable results can be because of an intermediatery page or some other
+process in between and might not be linked to our new change. 
+7. We should do an A/A test on our test customers periodically. 
